f3bd3fdebb6a92214bba460910b04eb6ed587381
Eric Garcia <garcia1999eric@gmail.com>
2021-09-28 18:27:41 -0500

    Sample Tile map

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
 
     private Vector2 moveDirection;
+    private bool facingRight = true;
 
     // Update is called once per frame
     void Update()
@@ -16,6 +17,12 @@ public class PlayerMovement : MonoBehaviour
         // Input
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
+
+        if((moveX < 0 && facingRight) || (moveX > 0 && !facingRight))
+        {
+            facingRight = !facingRight;
+            transform.Rotate(new Vector3(0, 180, 0));
+        }
 
         moveDirection = new Vector2(moveX, moveY).normalized;
     }
