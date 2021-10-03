6415d9e7161e27d87834185c2dbde38fc8439907
Michael McQuade <michael@giraffesyo.io>
2021-10-03 04:14:24 -0500

    Add Splash screen

---
+++ Assets/Scripts/SplashScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+public class SplashScreen : MonoBehaviour
+{
+    private float secondsToWait = 4.0f;
+    [SerializeField] private TextMeshProUGUI SlimeKingText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Sequence sq =  DOTween.Sequence().SetLoops((int)(secondsToWait / 2.0f));
+        sq.Append(SlimeKingText.transform.DOScale(new Vector3(1.2f,1.2f,1.0f), 1.0f));
+        sq.Append(SlimeKingText.transform.DOScale(new Vector3(1.0f,1.0f,1.0f), 2.0f));
+
+        StartCoroutine(ExitFromSplash());
+    }
+
+    private IEnumerator ExitFromSplash(){
+        yield return new WaitForSeconds(secondsToWait);
+        SceneManager.LoadSceneAsync("Menu");
+    }
+}
