53e8165beac7afaea49b9107c6043c322d3b2a39
Eric Garcia <garcia1999eric@gmail.com>
2021-10-08 21:28:53 -0500

    Ranged ai + ai melee attack

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -2,45 +2,129 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum AIType
+{
+    MELEE,
+    RANGED
+}
+
 public class AI : BaseCharacter
 {
     Transform playerPos;    // Transform object of the controllable player
+    public AIType aiMode;
+
+    Combat aiCombat;
+    int cooldownCounter = 0;
+    int attackCooldown;
+    float attackRange;
+    float playerRange;     // For ranged ai, how close the player will be before walking backwards
+    public LayerMask playerLayer;
+
 
     // Start is called before the first frame update
     protected new void Start()
     {
         base.Start();
-        setSpeed(2f);
+
+        aiCombat = GetComponent<Combat>();
         playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+        playerLayer = LayerMask.GetMask("Player");
+        if(aiMode == AIType.RANGED)
+        {
+            attackRange = 4f;
+            playerRange = 3.8f;
+            setSpeed(3f);
+            attackCooldown = 100;
+        }
+        else if(aiMode == AIType.MELEE)
+        {
+            attackRange = 1f;
+            setSpeed(2f);
+            attackCooldown = 50;
+        }
+    }
+
+    protected new void FixedUpdate()
+    {
+        base.FixedUpdate();
+        if (cooldownCounter != 0)
+        {
+            cooldownCounter = (cooldownCounter + 1) % attackCooldown;   // fixedUpdate gets called 50 times per second
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Move();
+        Move();
     }
-
 
     protected void Move()
     {
-        int moveX = 0;
-        int moveY = 0;
-        if(transform.localPosition.x < playerPos.localPosition.x)
-        {
-            moveX = 1;
+        moveX = 0;
+        moveY = 0;
+
+        Collider2D[] playerCollider = Physics2D.OverlapCircleAll(transform.position, attackRange, playerLayer);
+
+        if (aiMode == AIType.MELEE && cooldownCounter == 0) { // MELEE
+            if(playerCollider.Length != 0) // Within melee ranged
+            {
+                cooldownCounter = 1;
+                aiCombat.Attack();
+            }
+            if (attackCooldown != 0)
+            {
+                moveX = getMoveX();
+                moveY = getMoveY();
+                moveAttackPoint();
+            }
         }
-        else if(transform.position.x > playerPos.position.x)
-        {
-            moveX = -1;
-        }
-        if (transform.position.y < playerPos.position.y)
-        {
-            moveY = 1;
-        }
-        else if (transform.position.y > playerPos.position.y)
-        {
-            moveY = -1;
+        else if(aiMode == AIType.RANGED && playerCollider.Length != 0) // RANGED
+        { 
+            Transform playerTransform = playerCollider[0].GetComponent<Transform>();
+            float x1 = transform.position.x;
+            float y1 = transform.position.y;
+           
+            float x2 = playerTransform.position.x;
+            float y2 = playerTransform.position.y;
+            if(Distance(x1, y1, x2, y2) < playerRange)
+            {
+                moveX = getMoveX() * -1;
+                moveY = getMoveY() * -1;
+            }
         }
         base.Move(moveX, moveY);
+    }
+
+    float Distance(float x1, float y1, float x2, float y2)
+    {
+        return Mathf.Sqrt(Mathf.Pow(x2 - x1, 2) + Mathf.Pow(y2 - y1, 2)); ;
+    }
+
+    int getMoveX()
+    {
+        float diff = Mathf.Abs(transform.position.x - playerPos.position.x);
+        if (diff > 0.1f && transform.position.x < playerPos.position.x)
+        {
+            return 1;
+        }
+        else if (diff > 0.1f && transform.position.x > playerPos.position.x)
+        {
+            return -1;
+        }
+        return 0;
+    }
+    int getMoveY()
+    {
+        float diff = Mathf.Abs(transform.position.y - playerPos.position.y);
+        if (diff > 0.1f && transform.position.y < playerPos.position.y)
+        {
+            return 1;
+        }
+        else if (diff > 0.1f && transform.position.y > playerPos.position.y)
+        {
+            return -1;
+        }
+        return 0;
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,10 +12,6 @@ public class Player : BaseCharacter
     [SerializeField] private Vector3Int currentCell;
     private Vector2Int currentScreen;
 
-    public float moveX = 0;
-    public float moveY = 0;
-    public Transform attackPoint;
-
     // Start is called before the first frame update
     protected new void Start()
     {
@@ -51,14 +47,4 @@ public class Player : BaseCharacter
         }
     }
 
-    void moveAttackPoint()
-    {
-        if(moveX == 0 && moveY == 0){
-            attackPoint.localPosition = new Vector3(1, 0, 0);
-            return;
-        }
-        // Absoulte value used since when player turns left and right , the whole object gets rotated
-        // meaning we dont need to worry about placing attackPoint behind the object
-        attackPoint.localPosition = new Vector3(Mathf.Abs(moveX), moveY, 0);
-    }
 }
