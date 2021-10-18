c735639ebaeb6aab880449ac0ad05fad1d362766
Michael McQuade <michael@giraffesyo.io>
2021-10-17 19:14:12 -0500

    Remove old camera control logic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,17 +6,8 @@ using UnityEngine.InputSystem;
 public class Player : BaseCharacter
 {
     [SerializeField] private Transform spawnPoint;
-    public delegate void ScreenExitHandler(Vector2Int nextScreen);
-    public event ScreenExitHandler ScreenExited;
-    [SerializeField] private GridLayout gridLayout;
-    [SerializeField] private int xScreenSize = 20;
-    [SerializeField] private int yScreenSize = 20;
     [SerializeField] private float damageInvincibilitySeconds = 1.0f;
 
-
-    [SerializeField] private Vector3Int currentCell;
-
-    [SerializeField] private Vector2Int currentScreen;
     [SerializeField] private List<Ability> abilities = new List<Ability>();
 
     private SlimeKingActions slimeKingActions;
@@ -24,7 +15,6 @@ public class Player : BaseCharacter
     private InputAction aiming;
 
     private Vector2 aimingDirection = new Vector2();
-
 
     private void Awake()
     {
@@ -67,26 +57,11 @@ public class Player : BaseCharacter
     protected new void Start()
     {
         base.Start();
-        currentCell = getCurrentCell();
-        currentScreen = getCurrentScreen();
         abilities[(int)Ability.BasicAbilityKeys.Melee] = GetComponent<MeleeAbility>();
         abilities[(int)Ability.BasicAbilityKeys.Engulf] = GetComponent<EngulfAbility>();
         abilities[(int)Ability.BasicAbilityKeys.Ranged] = GetComponent<RangedAbility>();
 
 
-    }
-
-    private Vector2Int getCurrentScreen()
-    {
-        int x = (int)Mathf.Round((float)currentCell.x / xScreenSize);
-        int y = (int)Mathf.Round((float)currentCell.y / yScreenSize);
-        return new Vector2Int(x, y);
-    }
-
-    private Vector3Int getCurrentCell()
-    {
-
-        return gridLayout.WorldToCell(transform.position);
     }
 
 
@@ -95,16 +70,6 @@ public class Player : BaseCharacter
     {
         moveAttackPoint();
         Move(new Vector2(moveX, moveY));
-        currentCell = getCurrentCell();
-        Vector2Int nextScreen = getCurrentScreen();
-        if (currentScreen.x != nextScreen.x || currentScreen.y != nextScreen.y)
-        {
-            currentScreen = nextScreen;
-            if (ScreenExited != null)
-            {
-                ScreenExited.Invoke(nextScreen);
-            }
-        }
     }
 
     protected override void Die()
